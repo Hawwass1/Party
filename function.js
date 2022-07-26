6fc4bd4c8283a692b7c5587d9688b210ab232c94
Hawwass1 <suleimanhawwa.sh@gmail.com>
2022-07-26 20:18:56 +0100

    learning functions

---
+++ function.js
@@ -0,0 +1,9 @@
+function 
+function calculator(operation, num1, num2){
+    let result
+    if (operation.toLowerCase()==="add"){
+        result=num1+num2
+        console.log(result)
+    }
+}
+calculator("ADD,50,30")
