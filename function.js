144f815cf3e8aabd19b1bc75a9e288e99dd72184
Hawwass1 <suleimanhawwa.sh@gmail.com>
2022-07-30 18:15:13 +0100

    conditional statements

--- function.js
+++ function.js
@@ -20,41 +20,50 @@ function PrintScore(totalScore){
 var tot= score(5,2,12)
 PrintScore(tot)*/
 
-function TotalScore(CA1,CA2,exam){
-    if (CA1 <=20){
-        return CA1+CA2+exam
-    }
-    if (CA2 <=20){
-        return CA1+CA2+exam
-    }
-    if (exam<=60){
-        return CA1+CA2+exam
-    }
-    else{
-        console.log("Your input is invalid")
-    }
-
-}
 
 function Grade(FinalGrade){
   if (FinalGrade >= 70 && FinalGrade<=100){
-    console.log("Your grade is A")
-  }
-   else if (FinalGrade >=60 && FinalGrade<=69){
-    console.log("Your grade is B")   
-   }
-    else if (FinalGrade >=50 && FinalGrade<=59){
-    console.log("Your grade is C")
-  }
-    else if (FinalGrade >=45 && FinalGrade<=49){
-    console.log("Your grade is D")
+     console.log("Your grade is A")
+     }
+   else if (FinalGrade >=68 && FinalGrade<70){
+     console.log("Your grade is A")   
     }
-    else if (FinalGrade >=0 && FinalGrade<=44){
-    console.log("Your grade is F")
+    else if (FinalGrade >=60 && FinalGrade<70){
+     console.log("Your grade is B")
+     }
+    else if (FinalGrade >=58 && FinalGrade<60){
+     console.log("Your grade is B")
     }
+    else if (FinalGrade >=50 && FinalGrade<60){
+     console.log("Your grade is C")
+    }
+    else if (FinalGrade >=48 && FinalGrade<50){
+        console.log("Your grade is C")
+    }
+    else if (FinalGrade >=40 && FinalGrade<50){
+        console.log("Your grade is D")
+        }
+    else if (FinalGrade >=38 && FinalGrade<40){
+            console.log("Your grade is D")
+            }
+    else if (FinalGrade >=0 && FinalGrade<40){
+                console.log("Your grade is F")
+                }
     else{
         console.log("Your score is Invalid")
     } 
 }
-var Final= TotalScore(25,10,90)
+ function TotalScore(CA1,CA2,exam){
+    if (CA1,CA2 >=0 && CA1,CA2<=20){
+        return CA1+CA2+exam
+    }
+    else if (exam >=0 && exam <=100){
+        return CA1+CA2+exam
+    }
+    else {
+        console.log("Your score is invalid")
+    }
+
+}
+var Final= TotalScore(20,20,50)
 Grade(Final)
