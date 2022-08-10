68244782dda49e6e5ad0d0704289da9c5b0dbbae
Hawwass1 <suleimanhawwa.sh@gmail.com>
2022-08-10 23:12:21 +0100

    removed two buttons

--- event.js
+++ event.js
@@ -1,12 +1,4 @@
-function btn1(){
-    document.getElementById("btn2").style.backgroundColor = "cyan";
-    document.getElementById("btn2").style.borderRadius="50%";
-}
-function btn2(){
-    document.getElementById("btn3").style.backgroundColor = "pink";
-    document.getElementById("btn3").style.borderRadius="50%";
-}
-function btn3(){
-    document.getElementById("btn1").style.backgroundColor = "purple";
-    document.getElementById("btn1").style.borderRadius="50%";
+function btn(){
+    document.getElementById("background").style.backgroundColor = "cyan",;
+    document.getElementById("background")
 }
